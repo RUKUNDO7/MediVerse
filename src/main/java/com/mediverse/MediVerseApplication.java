03def17b17c7ade574cd5be5e5dd75bbef0a7437
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-19 10:29:09 +0200

    Modified backend entry point and added new files for integration testing and configuration.

--- src/main/java/com/mediverse/MediVerseApplication.java
+++ src/main/java/com/mediverse/MediVerseApplication.java
@@ -1,7 +1,11 @@
 package com.mediverse;
 
+import com.mediverse.model.Role;
+import com.mediverse.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +14,19 @@ public class MediVerseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediVerseApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner seedRoles(RoleRepository roleRepository) {
+		return args -> {
+			for (Role.RoleType roleType : Role.RoleType.values()) {
+				roleRepository.findByName(roleType).orElseGet(() -> {
+					Role role = new Role();
+					role.setName(roleType);
+					return roleRepository.save(role);
+				});
+			}
+		};
 	}
 
 }
